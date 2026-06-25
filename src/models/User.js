eb92529902a95dc711cf099271cc25ea9c1c809e
codeterna19 <codeterna19@gmail.com>
2026-06-25 14:18:28 +0530

    Fix password hashing pre-save hook to prevent TypeError: next is not a function and re-hashing on update

--- src/models/User.js
+++ src/models/User.js
@@ -29,9 +29,9 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
